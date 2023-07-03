091a39a6d1c3e9778b814a41d158f91ab31722f7
Maze <25077734+maze404@users.noreply.github.com>
2023-07-03 17:23:18 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,10 @@
 @echo ON
-if not exist C:\temp (
-    mkdir C:\temp
-)
+echo "Creating local temporary storage..."
+if not exist C:\temp ( mkdir C:\temp )
+echo "Done."
+echo "Copying the script files to local storage..."
 robocopy "%cd%\Data" C:\temp\Data /MIR
+echo "Done."
+echo "Starting the script..."
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& C:\temp\Data\cb-asi.ps1"
+pause
